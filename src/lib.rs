3441573a43c324471869b866137927b8bcf59bf1
r41ngee <146991131+r41ngee@users.noreply.github.com>
2026-09-25 19:09:27 +0300

    Add initial project files including .gitignore, Cargo.toml, Cargo.lock, and lib.rs with macros

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#[macro_export]
+macro_rules! outdir {
+    ($x: literal) => {
+        include!(concat!(env!("OUT_DIR"), $x))
+    };
+}
+
+#[macro_export]
+macro_rules! outdir_bytes {
+    ($x: literal) => {
+        include_bytes!(concat!(env!("OUT_DIR"), $x))
+    };
+}
+
+#[macro_export]
+macro_rules! outdir_str {
+    ($x: literal) => {
+        include_str!(concat!(env!("OUT_DIR"), $x))
+    };
+}
